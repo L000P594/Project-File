fbe91f62686aa6f42897e93b91dd53d2fb8451ad
EZZZZZZZZZ <eric0513.ryu@gmail.com>
2026-08-16 14:42:16 +0900

    디지털 시계 날짜 표시 형식 수정 및 코드 정리

--- DigitalClock/Script.js
+++ DigitalClock/Script.js
@@ -1,15 +1,23 @@
 const timetext = document.getElementById("time");
+const dateText = document.getElementById("date");
 
 function updateClock(){
     const now = new Date();
 
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
     timetext.textContent =
     hours + ":" + minutes + ":" + seconds;
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+
+    dateText.textContent =
+    year + "." + month + "." + day;
 }
 
 updateClock();
-setInterval(updateClock)
+setInterval(updateClock, 1000);
